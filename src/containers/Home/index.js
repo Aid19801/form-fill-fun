922684a110b09362e0614d49d5dfe0c79cdc8567
Adrian Thompson <adrian.thompson@infabode.com>
2019-02-11 21:23:01 +0000

    flexbox for responsiveness

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -5,8 +5,14 @@ class HomePage extends Component {
   render() {
     return (
       <div className="homepage">
-        <div className="header">header</div>
-        <div className="main-body">main body</div>
+
+        <div className="content-card">
+          <div className="header">header</div>
+          <div className="main-body">
+            <h1 className="title">About Your Activity</h1>
+          </div>
+        </div>
+
         <div className="footer">footer</div>
       </div>
     )
