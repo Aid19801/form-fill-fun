b17626691a02c5e32d2f0bf99f1d1271bba8ae72
Adrian Thompson <adrian.thompson@infabode.com>
2019-02-12 08:21:58 +0000

    added age selectors

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react'
+import Input from '@material-ui/core/Input';
+import NativeSelect from '@material-ui/core/Select';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './material-styles';
+
+import presentAges from '../../lib/presentAges';
+
 import './styles.css';
 
 class HomePage extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedAgeFrom: 0,
+      selectedAgeTo: 99,
+      listOfAges: [],
+    }
+  }
+
+  handleAgeFrom = event => {
+    this.setState({ selectedAgeFrom: event.target.value });
+  };
+
+  handleAgeTo = event => {
+    this.setState({ selectedAgeTo: event.target.value });
+  };
+
+  componentDidMount = () => {
+    this.setState({ listOfAges: presentAges() })
+  }
+
   render() {
+
+    const { classes } = this.props;
+    presentAges();
     return (
       <div className="homepage">
 
@@ -10,13 +42,58 @@ class HomePage extends Component {
           <div className="header">header</div>
           <div className="main-body">
             <h1 className="title">About Your Activity</h1>
+
+            <div className="each-input-container">
+              <h4 className="sub-title">Activity Name</h4>
+              
+              <div className="each-input">
+                <Input />
+              </div>
+
+            </div>
+
+            <div className="each-input-container">
+              <h4 className="sub-title">Recommended Age</h4>
+
+              <div className="age-selectors-container">
+                <div className="age-selector">
+                  <NativeSelect
+                    native
+                    value={this.state.ageFrom}
+                    onChange={(e) => this.handleAgeFrom(e)}
+                  >
+                  { this.state.listOfAges.map(((each, i) => {
+                    return <option key={i} value={each}>{each}</option>
+                  })) }
+                  </NativeSelect>
+                </div>
+
+                <div className="age-selector">
+                  <NativeSelect
+                    native
+                    value={this.state.ageFrom}
+                    onChange={(e) => this.handleAgeTo(e)}
+                  >
+                  { this.state.listOfAges.map(((each, i) => {
+                    return <option key={i} value={each}>{each}</option>
+                  })) }
+                  </NativeSelect>
+                </div>
+
+              </div>
+              
+            </div>
+
           </div>
         </div>
 
-        <div className="footer">footer</div>
+        <div className="footer">
+          <button onClick={null}>Back</button>
+          <button onClick={null}>Next</button>
+        </div>
       </div>
     )
   }
 }
 
-export default HomePage;
+export default withStyles(styles)(HomePage);
---
+++ src/lib/presentAges.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const presentAges = () => {
+    let arr = [];
+    for (let i = 0; i < 100; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+export default presentAges;
